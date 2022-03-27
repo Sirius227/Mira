4a7976fa5eba37bd54a5aeb724ce285742065bec
Sirius227 <76209670+Sirius227@users.noreply.github.com>
2022-03-28 01:25:36 +0300

    Update BotConfig.cs

--- DataStructs/BotConfig.cs
+++ DataStructs/BotConfig.cs
@@ -3,7 +3,6 @@
     public class BotConfig
     {
         public string? DiscordToken { get; set; }
-        public string? GeniusToken { get; set; }
         public string? WebhookToken { get; set; }
         public ulong WebhookId { get; set; }
         public string? GameStatus { get; set; }
